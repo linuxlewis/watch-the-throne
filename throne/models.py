bb06114b5f9caae33e3280de0615b6aa2eebb7e8
Sam Bolgert <sbolgert@gmail.com>
2014-11-24 22:37:56 -0800

    Add Bathroom.last_cycle_time and .cool_down property
    Add unittest coverage for new properties and .available.

--- throne/models.py
+++ throne/models.py
@@ -1,9 +1,11 @@
 import os
+
+from django.db import models
+from django.db.models import Q
+
 from tesselapi.settings import BASE_DIR
 
 from events.models import EnvironmentEvent
-
-from django.db import models
 
 
 class Bathroom(models.Model):
@@ -16,3 +18,32 @@ class Bathroom(models.Model):
     @property
     def available(self):
         return self.device.environmentevent_set.latest('created_on').event_type == 'lights-off'
+
+    @property
+    def cool_down(self):
+        # half life of the last trip % 10
+        minutes = self.last_cycle_time.total_seconds() / 60
+        return minutes / 2 % 10
+
+    @property
+    def last_cycle_time(self):
+        '''
+        returns timedelta between last 'lights-off' event with the previous
+        'lights-on' events.
+        '''
+
+        last_5_events = self.device.environmentevent_set.filter(Q(event_type='lights-off') | Q(event_type='lights-on')).order_by('-created_on').all()[:5]
+        last_5_events = list(last_5_events)
+
+        try:
+            assert len(last_5_events) >= 3
+        except AssertionError:
+            return None
+
+        end_event = last_5_events.pop(0)
+        # if currently mid cycle
+        if end_event.event_type == 'lights-on':
+            end_event = last_5_events.pop(0)
+        start_event = last_5_events.pop(0)
+
+        return end_event.created_on - start_event.created_on
--- throne/tests.py
+++ throne/tests.py
@@ -1,3 +1,41 @@
+from datetime import timedelta
+
 from django.test import TestCase
 
 # Create your tests here.
+
+from events.models import Device, EnvironmentEvent
+from throne.models import Bathroom
+
+
+class BathroomTestCase(TestCase):
+
+    def setUp(self):
+        # make a device
+        d = Device.objects.create(device_guid='some-lengthy-text')
+        # make a bathroom
+        self.bathroom = Bathroom.objects.create(name='Test Room', device=d)
+
+    def test_available(self):
+        # make  events
+        start = EnvironmentEvent()
+        start.event_type = 'lights-off'
+        start.device = self.bathroom.device
+        start.save()
+
+        self.assertEqual(self.bathroom.available, True)
+
+    def test_last_cycle(self):
+        # make  events
+        EnvironmentEvent.objects.create(event_type='lights-off', device=self.bathroom.device)
+        start = EnvironmentEvent.objects.create(event_type='lights-on', device=self.bathroom.device)
+        end = EnvironmentEvent.objects.create(event_type='lights-off', device=self.bathroom.device)
+
+        self.assertEqual(self.bathroom.last_cycle_time, end.created_on - start.created_on)
+
+    def test_cool_down(self):
+        # make  events
+        EnvironmentEvent.objects.create(event_type='lights-off', device=self.bathroom.device)
+        EnvironmentEvent.objects.create(event_type='lights-on', device=self.bathroom.device)
+        EnvironmentEvent.objects.create(event_type='lights-off', device=self.bathroom.device)
+        self.assertTrue(self.bathroom.cool_down)
