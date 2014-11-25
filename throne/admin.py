c39111f8c8fd40b8725a3b974bd59b7382330b86
Sam Bolgert <sbolgert@gmail.com>
2014-11-25 10:47:32 -0800

    Add BathroomAlertAdmin model to admin.py

--- throne/admin.py
+++ throne/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from throne.models import Bathroom
+from throne.models import Bathroom, BathroomAlert
+
 
 @admin.register(Bathroom)
 class BathroomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BathroomAlert)
+class BathroomAlertAdmin(admin.ModelAdmin):
     pass
